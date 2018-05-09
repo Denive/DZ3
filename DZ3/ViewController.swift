4cf3d4133668454f7c495210b5dbb1433cf03c7e
Denive <olyinyk.volodymyr@gmail.com>
2018-05-09 18:17:17 +0300

    add task2

--- DZ3/ViewController.swift
+++ DZ3/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        stringHardTask2()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    Создать метод который принимает как аргумент строку.
+//    Метод выводит строку зеркально, например Ось -> ьсО, Привет -> тевирП. не используя reverse (сделать алгоритм самому посимвольно)
+    func stringHardTask2() {
+        print(reverse(string: "Привет"))
     }
 
+    func reverse(string: String) -> String {
+        var result: String = ""
 
+        for ch in string {
+            result.insert(ch, at: result.startIndex)
+        }
+
+        return result
+    }
 }
 
