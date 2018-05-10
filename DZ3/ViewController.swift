78ae2364af7d147c6699aa39aeb815cc74f99d1d
Denive <olyinyk.volodymyr@gmail.com>
2018-05-10 18:40:30 +0300

    add task3

--- DZ3/ViewController.swift
+++ DZ3/ViewController.swift
@@ -15,12 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         stringHardTask2()
+        stringHardTask3()
     }
 
 //    Создать метод который принимает как аргумент строку.
 //    Метод выводит строку зеркально, например Ось -> ьсО, Привет -> тевирП. не используя reverse (сделать алгоритм самому посимвольно)
     func stringHardTask2() {
         print(reverse(string: "Привет"))
+    }
+
+    func stringHardTask3() {
+        print(format(number: "12345"))
+    }
+
+//    Добавить запятые в строку как их расставляет калькулятор
+//    1234567 -> 1,234,567 12345 -> 12,345 (не использовать встроенный метод для применения формата)
+    func format(number: String) -> String {
+        var result = ""
+
+        for (index, element) in number.reversed().enumerated() {
+            result.insert(element, at: result.startIndex)
+
+            if((index + 1) % 3 == 0 && index != number.count - 1) {
+                result.insert(",", at: result.startIndex)
+            }
+        }
+
+        return result
     }
 
     func reverse(string: String) -> String {
