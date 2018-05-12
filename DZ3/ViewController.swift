0fd55f9f1b477a85bdefa108276c6dbb398527f3
Denive <olyinyk.volodymyr@gmail.com>
2018-05-12 11:08:32 +0300

    add task1

--- DZ3/ViewController.swift
+++ DZ3/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionHardTask1()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    //    Создать метод который принимает 2 аргумента: массив строк и просто строку.
+    //    Метод возвращает true или false в зависимости есть ли данный элемент (тот второй аргумент,
+    //    который строка) в массиве (тот первый аргумент, который массив строк). например массив let array = [“one”, “two”]
+    //    и если передать в этот метод “one“ то должно вернуть true а если передать “three” то должно вернуть false
+    func collectionHardTask1() {
+        print(contains(in: ["one", "two"], value: "one"))
     }
 
+    func contains(in array: [String], value:String) -> Bool {
+        return array.contains(value)
+    }
 
 }
 
